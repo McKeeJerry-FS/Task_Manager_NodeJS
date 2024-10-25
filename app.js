da7ad452991aa3b9be4cd230f2c54daef129bb0c
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-10-25 17:16:12 -0400

    basic setup

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 3000;  
 
+
+// routes
+//Homepage
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Task Manager App');
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// getAllTasks app.get('/api/v1/tasks')
+// getById app.get('/api/v1/tasks/:id')
+// createTask app.post('/api/v1/tasks')
+// updateTask app.put('/api/v1/tasks/:id')
+// deleteTask app.delete('/api/v1/tasks/:id')
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
 });
