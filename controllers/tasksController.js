6e923b8d6dda8d8c20dc7e69a67fd34efa08a095
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-10-28 12:27:59 -0400

    refactored the code to clean up

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,47 +1,26 @@
 const Task = require('../models/task'); 
-const swal = require('sweetalert2');
+const asyncWrapper = require('../middleware/async');
 
-// getAllTasks app.get('/api/v1/tasks')
-// getById app.get('/api/v1/tasks/:id')
-// createTask app.post('/api/v1/tasks')
-// updateTask app.put('/api/v1/tasks/:id')
-// deleteTask app.delete('/api/v1/tasks/:id')
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks, amount: tasks.length });
+});
 
-const getAllTasks = async (req, res) => {
-    try{
-        const tasks = await Task.find({});
-        res.status(200).json({ tasks, amount: tasks.length });
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-}
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+});
 
-const createTask = async (req, res) => {
-    try{
-        const task = await Task.create(req.body);
-        res.status(201).json({ task });
-        swal.fire('Task created', 'Your task has been created', 'success');
-    }
-    catch(error){
-        res.status(500).json({msg: error});
-    }
-}
-
-const getTask = async (req, res) => { 
-    try{
+const getTask = asyncWrapper( async (req, res) => { 
         const { id: taskID } = req.params;
         const task = await Task.findOne({ _id: taskID });
         res.status(200).send({task});
         if (!task) {
             return res.status(404).json({msg: `Task with id ${taskID} not found`});
         }
-    } catch (error) {
-        res.status(500).json({msg: error});
-    }
-};
+});
 
-const updateTask = async (req, res) => {
-    try {
+const updateTask = asyncWrapper( async (req, res) => {
         const { id: taskID } = req.params;
         const task = await Task.findOneAndUpdate({_id: taskID,}, req.body, {
             new: true,
@@ -51,26 +30,17 @@ const updateTask = async (req, res) => {
             return res.status(404).json({msg: `Task with id ${taskID} not found`});
         }
         res.status(200).json({task});
-    } 
-    catch (error) {
-        res.status(500).json({msg: error});
-    }
-};
+});
 
-const deleteTask = async (req, res) => {
-    try{
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskID });
-        if(!task){
-            return res.status(404).json({msg: `Task with id ${taskID} not found`});
-        }
-        // res.status(200).json({task});
-        res.status(200).json({task: null, status: 'success'});
+const deleteTask = asyncWrapper( async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if(!task){
+        return res.status(404).json({msg: `Task with id ${taskID} not found`});
     }
-    catch{
-        res.status(404).json({msg: `Task with id ${taskID} not found`});
-    }
-};
+    // res.status(200).json({task});
+    res.status(200).json({task: null, status: 'success'});
+});
 
 module.exports = {
     getAllTasks,
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,4 +1,4 @@
-// custome error handling for routes that do not exist
+// custom error handling for routes that do not exist
 
 const notFound = (req, res, next) => {
     res.status(404).send('Route does not exist');
